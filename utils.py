9c52c33256cba3a152a7844ab5767c9edf9e72de
Piyush <piyushnags@gmail.com>
2023-04-04 21:03:41 -0400

    removed unnecessary preprocessing

--- utils.py
+++ utils.py
@@ -55,11 +55,6 @@ class DroneDataset(Dataset):
         self.transforms = transforms
         self.preprocess = T.Compose([
             T.ToTensor(),
-            T.Resize((320, 320)),
-            T.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]
-            )
         ])
     
 
